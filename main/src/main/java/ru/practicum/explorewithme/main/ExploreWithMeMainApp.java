c5c7cd38d8294f4c3ad328af80eb80917ee5791b
shishkin <alexconermail@gmail.com>
2025-05-23 02:12:59 +0300

    add stat server and dto

---
+++ main/src/main/java/ru/practicum/explorewithme/main/ExploreWithMeMainApp.java
@@ -0,0 +1,11 @@
+package ru.practicum.explorewithme.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeMainApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeMainApp.class, args);
+    }
+}
