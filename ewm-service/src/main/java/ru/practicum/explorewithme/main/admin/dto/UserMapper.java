540c1204e6a84a64f52eb38780ae61fb5e3fe087
shishkin <alexconermail@gmail.com>
2025-05-28 00:17:22 +0300

    admin/users service

---
+++ ewm-service/src/main/java/ru/practicum/explorewithme/main/admin/dto/UserMapper.java
@@ -0,0 +1,22 @@
+package ru.practicum.explorewithme.main.admin.dto;
+
+import lombok.experimental.UtilityClass;
+import ru.practicum.explorewithme.main.admin.model.User;
+
+@UtilityClass
+public class UserMapper {
+    public UserRespDto fromUser(User user) {
+        return UserRespDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .build();
+    }
+
+    public User toUser(UserReqDto dto) {
+        User user = new User();
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        return user;
+    }
+}
---
+++ ewm-service/src/main/java/ru/practicum/explorewithme/main/admin/dto/UserReqDto.java
@@ -0,0 +1,18 @@
+package ru.practicum.explorewithme.main.admin.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserReqDto {
+
+    @NotEmpty(message = "Имя пользователя не может быть пустым")
+    private String name;
+
+    @NotEmpty(message = "Email пользователя не может быть пустым")
+    @Email(message = "Некорректный email")
+    private String email;
+}
