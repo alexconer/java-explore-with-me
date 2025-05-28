c80c2bc013f1eb5ac203a09e38eef7c852f63b1d
shishkin <alexconermail@gmail.com>
2025-05-29 00:27:35 +0300

    category service

---
+++ ewm-service/src/main/java/ru/practicum/explorewithme/main/category/controller/admin/CategoryAdminController.java
@@ -0,0 +1,47 @@
+package ru.practicum.explorewithme.main.category.controller.admin;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import ru.practicum.explorewithme.main.category.dto.CategoryDto;
+import ru.practicum.explorewithme.main.category.service.CategoryService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/categories")
+@Slf4j
+public class CategoryAdminController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CategoryDto createCategory(@Valid @RequestBody CategoryDto categoryDto) {
+        log.info("Получен запрос на создание категории с данными: {}", categoryDto);
+        return categoryService.createCategory(categoryDto);
+    }
+
+    @PatchMapping("/{catId}")
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDto updateCategory(@PathVariable Long catId, @Valid @RequestBody CategoryDto categoryDto) {
+        log.info("Получен запрос на обновление категории с данными: {}", categoryDto);
+        return categoryService.updateCategory(catId, categoryDto);
+    }
+
+    @DeleteMapping("/{catId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCategory(@PathVariable Long catId) {
+        log.info("Получен запрос на удаление категории с id={}", catId);
+        categoryService.deleteCategory(catId);
+    }
+
+}
