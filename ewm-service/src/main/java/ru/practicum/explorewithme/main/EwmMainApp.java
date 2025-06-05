f117c42d2965bbff7b22c716e075f87d8ec6d868
shishkin <alexconermail@gmail.com>
2025-06-05 10:53:03 +0300

    fix maven errors

--- ewm-service/src/main/java/ru/practicum/explorewithme/main/EwmMainApp.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/main/EwmMainApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"ru.practicum.explorewithme.statclient", "ru.practicum.explorewithme.main"})
+@ComponentScan(basePackages = {"ru.practicum.explorewithme.statclient", "ru.practicum.explorewithme.main.", "ru.practicum.explorewithme.main"})
 public class EwmMainApp {
     public static void main(String[] args) {
         SpringApplication.run(EwmMainApp.class, args);
