da9d2cd5356999e8cc3e781c8c9aab0a7133e754
shishkin <alexconermail@gmail.com>
2025-06-11 20:13:16 +0300

    fix github style

--- ewm-service/src/main/java/ru/practicum/explorewithme/main/user/dto/UserReqDto.java
+++ ewm-service/src/main/java/ru/practicum/explorewithme/main/user/dto/UserReqDto.java
@@ -3,7 +3,6 @@ package ru.practicum.explorewithme.main.user.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
